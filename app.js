d24f27012ceff34c6463e8e64de39e34a2b71c4c
Alex Colombo <uricaine@hotmail.com>
2019-01-01 19:01:12 +0100

    new

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const socketio = require("socket.io");
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(7200, () => {
+const expressServer = app.listen(7200, () => {
   console.log("listen on port 7200");
 });
+
+socketio(expressServer);
